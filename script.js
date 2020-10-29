5979fb5a0739d74643701917ca3b3b6293507a96
Felipe Santiago <felipesantiago@usp.br>
2020-10-28 23:28:05 -0300

    Modificações pequenas

---
+++ script.js
@@ -0,0 +1,129 @@
+var botoes = document.querySelectorAll("button")
+var visor = document.querySelector("#visor")
+
+
+botoes[0].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 1
+
+});
+
+botoes[1].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 2
+
+});
+
+botoes[2].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 3
+
+});
+
+botoes[3].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 4
+
+});
+
+botoes[4].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 5
+
+});
+
+botoes[5].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 6
+
+});
+
+botoes[6].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 7
+
+});
+
+botoes[7].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 8
+
+});botoes[8].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 9
+
+});
+
+botoes[9].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 0
+
+});
+
+botoes[10].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = "+"
+
+});
+
+botoes[11].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = "-"
+
+});
+
+botoes[12].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = "*"
+
+});
+
+botoes[13].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = "/"
+
+});
+
+/*botoes[14].addEventListener("click", function() {
+    
+    
+    numberOne = visor.appendChild(document.createElement("h1"))
+    numberOne.innerText = 
+
+});*/
+
+
+botoes[15].addEventListener("click", function() {
+    
+    visor.innerText = ""
+    
+
+})
